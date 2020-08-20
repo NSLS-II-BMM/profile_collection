69cbbe2db7d11277259f0a7936e980afb9394343
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-19 22:48:34 -0400

    Updates to CI to make it compatible with 2020-2.0
    - Use a branch collection-2020-2.0rc7-1 of profile-collection-ci
    - The beamline-specific metapackages are not used since 2020-2.0 deployment
    - Parametrize the startup dir better

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,4 +2,5 @@
 
 # cp -v <...> ~/.ipython/profile_${TEST_PROFILE}/
 
-conda install -y -c ${CONDA_CHANNEL_NAME} 06-bm-bmm-collection
+# The beamline-specific metapackages are not used since 2020-2.0 deployment.
+# conda install -y -c ${CONDA_CHANNEL_NAME} 06-bm-bmm-collection
