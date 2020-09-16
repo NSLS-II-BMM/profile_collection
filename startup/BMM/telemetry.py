106643c090a7737062d6fe608873375700ed1a5f
Bruce Ravel <bravel@bnl.gov>
2020-09-16 09:44:40 -0400

    actually remember to commit the telemetry stuff this time....

---
+++ startup/BMM/telemetry.py
@@ -0,0 +1,153 @@
+from databroker import catalog
+from databroker.queries import TimeRange
+import numpy, json, os, time
+
+from BMM.periodictable import element_symbol, edge_energy, Z_number
+
+from IPython import get_ipython
+user_ns = get_ipython().user_ns
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+        
+
+
+class BMMTelementry():
+    '''A class for figuring out the historical average overhead for an XAFS scan at BMM
+
+    "Overhead" is defined as the excess time beyond the measurement
+    time.  The time span of the scan is computed from the time stamps
+    in the start and stop document.  The measurement time is the sum
+    of the dwell time column in the datatable from the measurement.
+    '''
+    def __init__(self):
+        self.folder = os.path.join(os.getenv('HOME'), '.ipython', 'profile_collection', 'startup', 'telemetry')
+        self.json = os.path.join(self.folder, 'telemetry.json')
+        self.bc = catalog['bmm']
+        self.start_date = '2019-09-01'
+        self.element = 'Dy'
+        self.reliability = 10
+        ###                         k-edges               l-edges
+        self.all_elements = list(range(22, 46)) + list(range(55, 93))
+
+        
+    def number(self):
+        db = user_ns['db']
+        query = TimeRange(since=self.start_date, until='2040')
+        time_search = self.bc.search(query)
+        xafs_search = time_search.search({'XDI._kind':'xafs'})
+        element_search = xafs_search.search({'XDI.Element.symbol': self.element})
+        print(f'Number of records for {self.element} since {self.start_date}: {len(element_search)}')
+        return(element_search)
+        
+    def scaling_factor(self):
+        db = user_ns['db']
+        element_search = self.number()
+        ratio, difference, dpp = numpy.array([]), numpy.array([]), numpy.array([])
+        l = len(list(element_search))
+        if l == 0:
+            return(0,0,0)
+        count = 0
+        for i,u in enumerate(list(element_search)):
+            this=db.v2[u]
+            if 'primary' in this.metadata['stop']['num_events']:
+                if this.metadata['start']['num_points'] != this.metadata['stop']['num_events']['primary']:
+                    continue
+            try:
+                t = this.primary.read()['dwti_dwell_time']
+                measurement_time = float(t.sum())
+                elapsed_time = this.metadata['stop']['time'] - this.metadata['start']['time']
+
+                ## worry about beam dumps and such skewing the statistics
+                if elapsed_time/measurement_time > 3:
+                    continue
+
+                difference = numpy.append(difference, (elapsed_time - measurement_time))
+                ratio = numpy.append(ratio, elapsed_time/measurement_time)
+                diff_per_point = (elapsed_time - measurement_time) / len(t)
+                dpp = numpy.append(dpp, diff_per_point)
+                count = count + 1
+            except:
+                pass
+            printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        #ratio = ratio / count
+        #difference = difference / count
+        #dpp = dpp/count
+        #return(count, ratio, difference, dpp)
+
+        return({'count': count,
+                'ratio': [ratio.mean(), ratio.std()],
+                'difference': [difference.mean(), difference.std()],
+                'dpp': [dpp.mean(), dpp.std()],
+            })
+    
+
+    def periodic_table(self, el=None):
+        start = time.time()
+        results = {}
+        ###             k-edges               l-edges
+        elements = list(range(22, 46)) + list(range(55, 93))
+        for z in self.all_elements:
+            el = element_symbol(z)
+            self.element = el
+            results[el] = self.scaling_factor()
+            
+        j = json.dumps(results)
+        f = open(self.json,"w")
+        f.write(j)
+        f.close()
+        end = time.time()
+        print('\n\nThat took %.1f min' % ((end-start)/60))
+        
+
+    def interpolate(self, energy):
+        a = json.load(open(self.json))
+        e, t = numpy.array([]), numpy.array([])
+        for z in self.all_elements:
+            if 'count' not in a[element_symbol(z)]:
+                continue
+            if a[element_symbol(z)]['count'] < self.reliability:
+                continue
+            t = numpy.append(t, a[element_symbol(z)]['dpp'][0])
+            if z < 46:
+                e=numpy.append(e, edge_energy(z, 'k'))
+            else:
+                e=numpy.append(e, edge_energy(z, 'l3'))
+                             
+        s=numpy.argsort(e)
+        return(numpy.interp(energy, e[s], t[s]))
+
+    def overhead_per_point(self, element, edge=None):
+        a = json.load(open(self.json))
+        element = element_symbol(element)
+        if edge is not None and edge.lower() in ('l2', 'l1'):
+            return([self.interpolate(edge_energy(element, edge)), 0])
+        if element in a and 'dpp' in a[element]:
+            return(a[element]['dpp'])
+        else:
+            if edge is None or edge.lower() not in ('l2', 'l1'):
+                edge = 'k'
+                if Z_number(element) > 45:
+                    edge = 'l3'
+            return([self.interpolate(edge_energy(element, edge)), 0])
+        
---
+++ startup/BMM/xafs_functions.py
@@ -0,0 +1,221 @@
+
+from BMM.functions     import error_msg, warning_msg, go_msg, url_msg, bold_msg, verbosebold_msg, list_msg, disconnected_msg, info_msg, whisper
+from BMM.functions     import countdown, boxedtext, now, isfloat, inflect, e2l, etok, ktoe
+import numpy
+
+from IPython import get_ipython
+user_ns = get_ipython().user_ns
+
+CS_BOUNDS     = [-200, -30, 15.3, '14k']
+CS_STEPS      = [10, 0.5, '0.05k']
+CS_TIMES      = [0.5, 0.5, '0.25k']
+CS_MULTIPLIER = 0.72
+
+
+def sanitize_step_scan_parameters(bounds, steps, times):
+    '''Attempt to identify and flag/correct some common scan parameter mistakes.'''
+    problem = False
+    text = ''
+
+    ############################################################################
+    # bounds is one longer than steps/times, length of steps = length of times #
+    ############################################################################
+    if (len(bounds) - len(steps)) != 1:
+        text += error_msg('\nbounds must have one more item than steps\n')
+        text += error_msg('\tbounds = %s\n' % ' '.join(map(str, bounds)))
+        text += error_msg('\tsteps = %s\n'  % ' '.join(map(str, steps)))
+        problem = True
+    if (len(bounds) - len(times)) != 1:
+        text += error_msg('\nbounds must have one more item than times\n')
+        text += error_msg('\tbounds = %s\n' % ' '.join(map(str, bounds)))
+        text += error_msg('\ttimes = %s\n'  % ' '.join(map(str, times)))
+        problem = True
+
+    ############################
+    # tests of boundary values #
+    ############################
+    for b in bounds:
+        if not isfloat(b) and b[-1:].lower() == 'k':
+            if not isfloat(b[:-1]):
+                text += error_msg('\n%s is not a valid scan boundary value\n' % b)
+                problem = True
+        elif not isfloat(b):
+            text += error_msg('\n%s is not a valid scan boundary value\n' % b)
+            problem = True
+
+        if not isfloat(b) and b[:1] == '-' and b[-1:].lower() == 'k':
+            text += error_msg('\nNegative bounds must be energy-valued, not k-valued (%s)\n' % b) 
+            problem = True
+               
+    #############################
+    # tests of step size values #
+    #############################
+    for s in steps:
+        if not isfloat(s) and s[-1:].lower() == 'k':
+            if not isfloat(s[:-1]):
+                text += error_msg('\n%s is not a valid scan step size value\n' % s)
+                problem = True
+            elif float(s[:-1]) < 0:
+                text += error_msg('\nStep sizes cannot be negative (%s)\n' % s)
+                problem = True
+        elif not isfloat(s):
+            text += error_msg('\n%s is not a valid scan step size value\n' % s)
+            problem = True
+
+        if isfloat(s) and float(s) < 0:
+            text += error_msg('\nStep sizes cannot be negative (%s)\n' % s)
+            problem = True
+        elif isfloat(s) and float(s) <= 0.09:
+            text += warning_msg('\n%s is a very small step size!\n' % s)
+        elif not isfloat(s) and s[-1:].lower() == 'k' and isfloat(s[-1:]) and float(s[:-1]) < 0.01:
+            text += warning_msg('\n%s is a very small step size!\n' % s)
+            
+                
+    ####################################
+    # tests of integration time values #
+    ####################################
+    for t in times:
+        if not isfloat(t) and t[-1:].lower() == 'k':
+            if not isfloat(t[:-1]):
+                text += error_msg('\n%s is not a valid integration time value\n' % t)
+                problem = True
+            elif float(t[:-1]) < 0:
+                text += error_msg('\nIntegration times cannot be negative (%s)\n' % t)
+                problem = True
+        elif not isfloat(t):
+            text += error_msg('\n%s is not a valid integration time value\n' % t)
+            problem = True
+
+        if isfloat(t) and float(t) < 0:
+            text += error_msg('\nIntegration times cannot be negative (%s)\n' % t)
+            problem = True
+        elif isfloat(t) and float(t) <= 0.1:
+            text += warning_msg('\n%s is a very short integration time!\n' % t)
+        elif not isfloat(t) and t[-1:].lower() == 'k' and isfloat(t[-1:]) and float(t[:-1]) < 0.05:
+            text += warning_msg('\n%s is a very short integration time!\n' % t)
+
+    if text:
+        text += error_msg('\nsee ') + url_msg('https://nsls-ii-bmm.github.io/BeamlineManual/xafs.html#scan-regions\n')
+
+            
+    return problem, text
+    
+
+
+def conventional_grid(bounds=CS_BOUNDS, steps=CS_STEPS, times=CS_TIMES, e0=7112, element=None, edge=None, ththth=False):
+    '''Input:
+       bounds:   (list) N relative energy values denoting the region boundaries of the step scan
+       steps:    (list) N-1 energy step sizes
+       times:    (list) N-1 integration time values
+       e0:       (float) edge energy, reference for boundary values
+       ththth:   (Boolean) using the Si(333) reflection
+    Output:
+       grid:     (list) absolute energy values
+       timegrid: (list) integration times
+       approximate_time: (float) a very crude estimate of how long in minutes the scan will take
+
+    Boundary values are either in eV units or wavenumber units.
+    Values in eV units are floats, wavenumber units are strings of the
+    form '0.5k' or '1k'.  String valued boundaries indicate a value to
+    be converted from wavenumber to energy.  E.g. '14k' will be
+    converted to 746.75 eV, i.e. that much above the edge energy.
+
+    Step values are either in eV units (floats) or wavenumber units
+    (strings).  Again, wavenumber values will be converted to energy
+    steps as appropriate.  For example, '0.05k' will be converted into
+    energy steps such that the steps are constant 0.05 invAng in
+    wavenumber.
+
+    Time values are either in units of seconds (floats) or strings.
+    If strings, the integration time will be a multiple of the
+    wavenumber value of the energy point.  For example, '0.5k' says to
+    integrate for a number of seconds equal to half the wavenumber.
+    So at 5 invAng, integrate for 2.5 seconds.  At 10 invAng,
+    integrate for 5 seconds.
+
+    Examples:
+       -- this is the default (same as (g,it,at) = conventional_grid()):
+       (grid, inttime, time) = conventional_grid(bounds=[-200, -30, 15.3, '14k'],
+                                                 steps=[10, 0.5, '0.05k'],
+                                                 times=[0.5, 0.5, '0.25k'], e0=7112)
+
+       -- more regions
+       (grid, inttime, time) = conventional_grid(bounds=[-200.0, -20.0, 30.0, '5k', '14.5k'],
+                                                 steps=[10.0, 0.5, 2, '0.05k'],
+                                                 times=[1, 1, 1, '1k'], e0=7112)
+
+       -- many regions, energy boundaries, k-steps
+       (grid, inttime, time) = conventional_grid(bounds=[-200, -30, -10, 15, 100, 300, 500, 700, 900],
+                                                 steps=[10, 2, 0.5, '0.05k', '0.05k', '0.05k', '0.05k', '0.05k'],
+                                                 times=[0.5, 0.5, 0.5, 1, 2, 3, 4, 5], e0=7112)
+
+       -- a one-region xanes scan
+       (grid, inttime, time) = conventional_grid(bounds=[-10, 40],
+                                                 steps=[0.25,],
+                                                 times=[0.5,], e0=7112)
+    '''
+    tele = user_ns['tele']
+    
+    if (len(bounds) - len(steps)) != 1:
+        return (None, None, None)
+    if (len(bounds) - len(times)) != 1:
+        return (None, None, None)
+    for i,s in enumerate(bounds):
+        if type(s) is str:
+            this = float(s[:-1])
+            bounds[i] = ktoe(this)
+    bounds.sort()
+
+    enot = e0
+    if ththth:
+        enot = e0/3.0
+        bounds = list(array(bounds)/3)
+    grid = list()
+    timegrid = list()
+    for i,s in enumerate(steps):
+        if type(s) is str:
+            step = float(s[:-1])
+            if ththth: step = step/3.
+            ar = enot + ktoe(numpy.arange(etok(bounds[i]), etok(bounds[i+1]), step))
+        else:
+            step = steps[i]
+            if ththth: step = step/3.
+            ar = numpy.arange(enot+bounds[i], enot+bounds[i+1], step)
+        grid = grid + list(ar)
+        grid = list(numpy.round(grid, decimals=2))
+        if type(times[i]) is str:
+            tar = etok(ar-enot)*float(times[i][:-1])
+        else:
+            tar = times[i]*numpy.ones(len(ar))
+        timegrid = timegrid + list(tar)
+        timegrid = list(numpy.round(timegrid, decimals=2))
+
+    if element is not None:
+        overhead, uncertainty = tele.overhead_per_point(element, edge)
+    else:
+        overhead = tele.interpolate(e0)
+        
+    approximate_time = (sum(timegrid) + float(len(timegrid))*overhead) / 60.0
+    delta = float(len(timegrid))*uncertainty / 60.0
+    return (grid, timegrid, round(approximate_time, 1), round(delta, 1))
+
+## -----------------------
+##  energy step scan plan concept
+##  1. collect metadata from an INI file
+##  2. compute scan grid
+##  3. move to center of angular range
+##  4. drop into pseudo channel cut mode
+##  5. set OneCount and Single modes on the detectors
+##  6. begin scan repititions, for each one
+##     a. scan:
+##          i. make metadata dict, set md argument in call to scan plan
+##         ii. move
+##        iii. set acquisition time for this point
+##         iv. trigger
+##          v. collect
+##     b. grab dataframe from Mongo
+##        http://nsls-ii.github.io/bluesky/tutorial.html#aside-access-saved-data
+##     c. write XDI file
+##  8. return to fixed exit mode
+##  9. return detectors to AutoCount and Continuous modes
+
