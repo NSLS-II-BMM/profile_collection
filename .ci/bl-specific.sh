78ceb33126e48c316972843df589e3177239f1dd
Abigail Giles <agiles@bnl.gov>
2021-03-23 13:06:27 -0400

    Update to use collection-2021-1.0 and -2021-1.2 in CI

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 
-# cp -v <...> ~/.ipython/profile_${TEST_PROFILE}/
-
-# The beamline-specific metapackages are not used since 2020-2.0 deployment.
-# conda install -y -c ${CONDA_CHANNEL_NAME} 06-bm-bmm-collection
