f34991e8f9e6f35c7f8ab31da89b132e89b5e69e
Maksim Rakitin <mrakitin@bnl.gov>
2021-03-29 16:24:50 -0400

    Remove redis installation/service start steps as it is handled by the CI steps now

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,11 +2,6 @@
 
 export AZURE_TESTING=1
 
-sudo apt-get update && sudo apt-get install redis
-
-sudo systemctl start redis
-sudo systemctl status redis
-
 if [ "$CONDA_ENV_NAME" == "collection-2021-1.0" ]; then
     conda remove wxpython --force -y
 fi
