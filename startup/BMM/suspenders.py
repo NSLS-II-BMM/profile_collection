4064a8fd5d36d4c1a8a9c9d337f99efddec256a1
BMM Operator <xf06bm@xf06bm-ws1.nsls2.bnl.local>
2021-06-29 07:50:39 -0400

    begin work on slack suspender messages

--- startup/BMM/suspenders.py
+++ startup/BMM/suspenders.py
@@ -39,8 +39,20 @@ except:
 
 ## ----------------------------------------------------------------------------------
 ## suspend if the experimental photon shutter closes, resume 5 seconds after opening
+from bluesky.plan_stubs import null
+from BMM.logging import post_to_slack
+def tell_slack_shb_closed():
+    yield from null()
+    print('triggering closed message')
+    post_to_slack('B shutter closed')
+def tell_slack_shb_opened():
+    yield from null()
+    print('triggering opened message')
+    post_to_slack('B shutter opened')
 try:
-    suspender_shb = SuspendBoolHigh(user_ns['shb'].state, sleep=5)
+    suspender_shb = SuspendBoolHigh(user_ns['shb'].state, sleep=5,
+                                    pre_plan=tell_slack_shb_closed,
+                                    post_plan=tell_slack_shb_opened)
     all_BMM_suspenders.append(suspender_shb)
 except:
     pass
