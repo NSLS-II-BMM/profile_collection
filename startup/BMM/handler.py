4d32cb398ec7141e051a05fecabed999df693107
Bruce Ravel <bravel@bnl.gov>
2021-11-17 15:48:03 -0500

    add handler.py

---
+++ startup/BMM/handler.py
@@ -0,0 +1,13 @@
+
+import numpy
+from PIL import Image
+
+class WEBCAM_JPEG_HANDLER:
+    def __init__(self, resource_path):
+        # resource_path is really a template string with a %d in it
+        self._template = resource_path
+
+    def __call__(self, index):
+        filepath = self._template % index
+        return numpy.asarray(Image.open(filepath))
+
