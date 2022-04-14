e170931fc4904cf3fc6e6df9db6728bfe9cbfaf1
Jakub Wlodek <jwlodek@bnl.gov>
2022-04-14 14:07:59 -0400

    Add some basic acceptance tests

---
+++ acceptance_tests/linescan.py
@@ -0,0 +1,3 @@
+def test_linescan():
+
+    RE(linescan(xafs_x, 'it', -1, 1, 3, force=True, pluck=False))
---
+++ acceptance_tests/slot.py
@@ -0,0 +1,3 @@
+def test_slot():
+    RE(slot(1))
+    print(f'xafs_wheel position is {xafs_wheel.position}, it should be -30')
