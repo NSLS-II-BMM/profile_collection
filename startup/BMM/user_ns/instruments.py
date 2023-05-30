5b51b80697343c316d63fa0c78b322d7252f9fed
Bruce Ravel <bravel@bnl.gov>
2023-05-30 08:56:57 -0400

    typo fix

--- startup/BMM/user_ns/instruments.py
+++ startup/BMM/user_ns/instruments.py
@@ -631,7 +631,7 @@ run_report('\tvideo recording via USB cameras')
 from BMM.video import USBVideo
 usbvideo1 = USBVideo('XF:06BM-ES{UVC-Cam:1}CV1:', name='usbvideo1')
 usbvideo1.path = '/nsls2/data/bmm/assets/usbcam/'
-usbvideo1.initialize()RE
+usbvideo1.initialize()
 
 # usbvideo2 = USBVideo('XF:06BM-ES{UVC-Cam:1}CV2:', name='usbvideo2')
 # usbvideo2.enable.put(0)
