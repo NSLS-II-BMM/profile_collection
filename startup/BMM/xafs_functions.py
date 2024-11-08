7d403e5667c2b3cdeebf00095e0a2a1360161243
Bruce Ravel <bravel@bnl.gov>
2024-11-08 08:35:33 -0500

    Deal with Ru

--- startup/BMM/xafs_functions.py
+++ startup/BMM/xafs_functions.py
@@ -211,7 +211,7 @@ def conventional_grid(bounds=CS_BOUNDS, steps=CS_STEPS, times=CS_TIMES, e0=7112,
         timegrid = timegrid + list(tar)
         timegrid = list(numpy.round(timegrid, decimals=2))
 
-    if element in ('Kr', 'Hg', 'Pu', 'Th'):
+    if element in ('Kr', 'Hg', 'Pu', 'Th', 'Ru'):
         overhead, uncertainty = tele.average()
     elif element is not None:
         overhead, uncertainty, maxdpp, mindpp = tele.overhead_per_point(element) #, edge)
